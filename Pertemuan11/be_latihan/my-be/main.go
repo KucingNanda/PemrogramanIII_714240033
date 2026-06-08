644c43716a93ce4a2b9e69e9f321be06f4b0a113
KucingNanda <nandasr.24@gmail.com>
2026-06-08 15:08:16 +0700

    P11

---
+++ Pertemuan11/be_latihan/my-be/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"be_latihan/config"
+	"be_latihan/model"
+	"be_latihan/router"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	// Init database
+	config.InitDB()
+
+	// Auto migrate
+	config.GetDB().AutoMigrate(&model.Mahasiswa{}, &model.User{})
+
+	// CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: strings.Join(config.GetAllowedOrigins(), ","),
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin,Content-Type,Accept,Authorization",
+	}))
+
+	// Routes
+	router.SetupRoutes(app)
+
+	app.Listen(":3000")
+}
